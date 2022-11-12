f334050ace6e0e59fd3df5fcd524b087979e907b
Joey Lent <supercolbat@gmail.com>
2022-11-12 16:41:53 -0600

    Create card view

--- lib/widgets/set_box.dart
+++ lib/widgets/set_box.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:harvesthacks2022/pages/flashcard_view.dart';
 
 class SetBox extends StatelessWidget {
   final String setName;
@@ -14,24 +15,38 @@ class SetBox extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(3),
-      width: 100,
-      height: 100,
-      color: const Color(0xFF6159D0),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(
-            setName,
-            style: const TextStyle(fontSize: 25),
+    return GestureDetector(
+      onTap: () {
+        Navigator.of(context).push(
+          MaterialPageRoute(
+            builder: (context) => FlashcardView(
+              terms: [
+                {"term": "sus", "definition": "mogus"},
+                {"term": "sus", "definition": "mogus"},
+              ],
+            ),
           ),
-          Text(
-            "$cardsAmount terms",
-            style: const TextStyle(fontSize: 12),
-          )
-        ],
+        );
+      },
+      child: Container(
+        padding: const EdgeInsets.all(3),
+        width: 100,
+        height: 100,
+        color: const Color(0xFF6159D0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              setName,
+              style: const TextStyle(fontSize: 25),
+            ),
+            Text(
+              "$cardsAmount terms",
+              style: const TextStyle(fontSize: 12),
+            )
+          ],
+        ),
       ),
     );
   }
