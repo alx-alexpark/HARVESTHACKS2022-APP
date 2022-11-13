c39debf1d77b4f7f7146688576043875b0532550
Alexander Park <al3xpark@proton.me>
2022-11-13 06:28:03 -0600

    make it so temperature can be changed in the api

--- lib/paraphrase_api.dart
+++ lib/paraphrase_api.dart
@@ -1,18 +1,29 @@
 import 'dart:convert';
 
+import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:harvesthacks2022/firebase_options.dart';
+import 'package:intl/number_symbols_data.dart';
 
 class ApiUtil {
+  static Map<int, double> temps = {1: 0.4, 2: 0.7, 3: 1};
+  static double currentTemp = 0.7;
+
   static Future<String> paraphrase(String input) async {
     // only reason im using firebase_options.dart is because it is a file already already in .gitignore.
     final queryParameters = {
       'text': input,
-      'token': DefaultFirebaseOptions.token
+      'token': DefaultFirebaseOptions.token,
+      'temp': currentTemp,
     };
     var url = Uri.https(
         'RefactoringAPI.bernygg.repl.co', '/paraphrase', queryParameters);
     final response = await http.get(url);
     return jsonDecode(response.body)["text"];
+  }
+
+  static void setTemp(int i) {
+    currentTemp = temps[i]!;
+    print("Temp changed");
   }
 }
