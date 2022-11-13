69ea7bc2e87de2893ea66f714f0f7f5a3cd506a0
Joey Lent <supercolbat@gmail.com>
2022-11-13 07:42:14 -0600

    Here goes an hour of our lives we will never get back.

--- lib/paraphrase_api.dart
+++ lib/paraphrase_api.dart
@@ -4,15 +4,40 @@ import 'package:http/http.dart' as http;
 import 'package:harvesthacks2022/firebase_options.dart';
 
 class ApiUtil {
+  static List<double> temps = [0.4, 0.7, 1];
+  static List<String> models = [
+    "text-ada-001",
+    "text-babbage-001",
+    "text-curie-001",
+    "text-davinci-002"
+  ];
+  static double currentTemp = 0.7;
+  static String currentModel = "text-curie-001";
+
   static Future<String> paraphrase(String input) async {
     // only reason im using firebase_options.dart is because it is a file already already in .gitignore.
     final queryParameters = {
+      'token': DefaultFirebaseOptions.token,
       'text': input,
-      'token': DefaultFirebaseOptions.token
+      'temp': currentTemp.toString(),
+      'model': currentModel.toString(),
     };
+    print('a');
     var url = Uri.https(
         'RefactoringAPI.bernygg.repl.co', '/paraphrase', queryParameters);
+    print('b');
     final response = await http.get(url);
+    print('c');
     return jsonDecode(response.body)["text"];
+  }
+
+  static void setTemp(int i) {
+    currentTemp = temps[i];
+    print("Temp changed");
+  }
+
+  static void setModel(int i) {
+    currentModel = models[i];
+    print("Model changed");
   }
 }
